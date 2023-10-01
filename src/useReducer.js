27fc37e3a38432362865af88214e978117e10e71
stella <stellamaia97@hotmail.com>
2023-10-01 02:18:05 -0300

    Boas práticas do UseReducer

--- src/useReducer.js
+++ src/useReducer.js
@@ -1,6 +1,31 @@
-import React, { useState, useReducer } from 'react';
-//Inicializando useContext
-function UseReducer() {
+//useReducer : É usado para gerenciar o estado complexo de um componente, especialmente quando este estado envolve múltiplas ações, 
+//É semelhante ao 'useState', mas geralmente é preferido quando o estado precisa de lógica mais avançada para ser atualizado.
+import React, { useReducer } from 'react';
+//Inicializando useReducer
+//inicializar variavel
 
+const initialState = { count: 0 };
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'increment':
+            return { count: state.count + 1 };
+        case 'decrement':
+            return { count: state.count - 1 };
+        default:
+            return state;
+    }
+
+}
+
+function UseReducer(){
+    const[state, dispatch] = useReducer(reducer, initialState);
+    return (
+        <div>
+            <p>Contagem:{state.count}</p>
+            <button onClick={() => dispatch({ type:'increment'})}>Incrementar</button>
+            <button onClick={() => dispatch({ type:'decrement'})}>Descrementar</button>
+        </div>
+    )
 }
 export default UseReducer;
