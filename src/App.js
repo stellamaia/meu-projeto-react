1cd29d5dace6c61967ee518d78da9d14fc5208c0
stella <stellamaia97@hotmail.com>
2023-09-30 22:35:47 -0300

    Implementação react-router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import './App.css';
+
+//configuraçao router
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import TodoList from './TodoList';
 import UseEffectExample from './UseEffectExample';
 
 
 function App() {
   return (
-    <div>
+    
+    <Router>
+      {/* <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/use-effect">UseEffect</Link></li>
+      </ul> */}
+      {/* Switch é onde vou definir as rotas */}
+      <Routes>
+      <Route path="/" element={<TodoList/>}></Route>
+        <Route path="/use-effect" element={<UseEffectExample/>}></Route>
+      </Routes>
+    </Router>
 
-
-      <TodoList />
-   <UseEffectExample />
-
-    </div>
   );
 }
 
