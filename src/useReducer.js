eaca76f75228b5b48065f6377f3a64c3c039137c
stella <stellamaia97@hotmail.com>
2023-10-02 23:28:35 -0300

    Estilização e implementação de buttons

--- src/useReducer.js
+++ src/useReducer.js
@@ -18,13 +18,57 @@ function reducer(state, action) {
 
 }
 
-function UseReducer(){
-    const[state, dispatch] = useReducer(reducer, initialState);
+function UseReducer() {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    const rootStyle = {
+        display: 'flex',
+        justifyContent: 'center',
+        position:'absolute',
+        top:'200px',
+    }
+    const containerStyle = {
+        margin: '10px',
+        padding: '10px',
+        border: '1px solid red',
+        borderRadius: '5px',
+        textAlign: 'center',
+        width:'70%',
+        
+    }
+    const textStyle = {
+
+        fontSize: '30px',
+        textAlign: 'center',
+    }
+    const buttonStyleIncrement = {
+        margin: '5px',
+        padding: '10px 20px',
+        fontSize: '16px',
+        backgroundColor: 'blue',
+        color: 'white',
+        borderRadius: '4px',
+        cursor: 'pointer',
+        
+   
+    };
+    const buttonStyleDecrement = {
+        margin: '5px',
+        padding: '10px 20px',
+        fontSize: '16px',
+        backgroundColor: 'red',
+        color: 'white',
+        borderRadius: '4px',
+        cursor: 'pointer',
+    };
+    
     return (
-        <div>
-            <p>Contagem:{state.count}</p>
-            <button onClick={() => dispatch({ type:'increment'})}>Incrementar</button>
-            <button onClick={() => dispatch({ type:'decrement'})}>Descrementar</button>
+        <div style={rootStyle}>
+        <div style={containerStyle}>
+            <p style={textStyle}>Contagem:{state.count}</p>
+            <button style={buttonStyleIncrement} onClick={() => dispatch({ type: 'increment' })}>Incrementar</button>
+            <button style={buttonStyleDecrement} onClick={() => dispatch({ type: 'decrement' })}>Descrementar</button>
+        </div>
         </div>
     )
 }
