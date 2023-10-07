af739f40a4293f63b4016f20e9387c58f909c282
stella <stellamaia97@hotmail.com>
2023-10-07 00:29:51 -0300

    teste

--- src/NavBar.js
+++ src/NavBar.js
@@ -8,7 +8,7 @@ function NavBar() {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary" style={navbarStyle}>
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">UseState</a>
+                <a className="navbar-brand" href="/">UseStates</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
