f70b4835647f018d590772ea7c6cf0d79a3c316c
stella <stellamaia97@hotmail.com>
2023-10-06 22:45:05 -0300

    Implementação de comentários

--- src/Crud.js
+++ src/Crud.js
@@ -1,8 +1,15 @@
-import React, {useState} from "react";
-import NavBar from "./NavBar";
- function UseState(){
-return (
+// import React, { createContext, useContext, useState, useEffect } from "react";
+// import axios from 'axios'
+// import NavBar from "./NavBar";
 
-)
-}
-export default Crud;
+// function Crud() {
+//     return (
+//         <div>
+//             <NavBar />
+//         {/* Renderizar formulário de edição/criação aqui */}
+//         <button onClick={handleSubmit}>Salvar</button>
+//         <button onClick={handleDelete}>Excluir</button>
+//         </div>
+//     )
+// }
+// export default Crud;
